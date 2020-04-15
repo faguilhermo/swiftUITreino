ea54dbc2d119dc344430988810e11f65035fb35b
Fabrício <guilhermo.fabricio@gmail.com>
2020-04-15 17:38:40 -0300

    Add custom buttons

--- treinandoSwiftUI/treinandoSwiftUI/ContentView.swift
+++ treinandoSwiftUI/treinandoSwiftUI/ContentView.swift
@@ -14,23 +14,40 @@ struct ContentView: View {
             VStack {
                 VStack {
                     Text("viagens")
-                        .foregroundColor(Color.white)
+                        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                         .font(.custom("Avenir Black", size: 20))
-                        .padding(.top, 40)
+                        .padding(.top, 50)
                     Text("ESPECIAL")
-                        .foregroundColor(Color.white)
+                        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                         .font(.custom("Avenir Book", size: 20))
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 20)
                     Text("BRASIL")
-                        .foregroundColor(Color.white)
+                        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                         .font(.custom("Avenir Black", size: 20))
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 20)
                 }
                 .frame(width: view.size.width, height: 180, alignment: .top)
-                .background(/*@START_MENU_TOKEN@*/Color.purple/*@END_MENU_TOKEN@*/)
-                .edgesIgnoringSafeArea(.all)
+                .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Hotéis")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    }
+                    .frame(width: 100, height: 50)
+                    .background(Color(#colorLiteral(red: 0.2113958895, green: 0.03864551708, blue: 0.5749527812, alpha: 1)))
+
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    }
+                    .frame(width: 100, height: 50)
+                    .background(Color(#colorLiteral(red: 0.2113958895, green: 0.03864551708, blue: 0.5749527812, alpha: 1)))
+                }
 
                 List {
                     Text("São Paulo")
@@ -39,6 +56,7 @@ struct ContentView: View {
                     Text("Ceará")
                 }
             }
+            .edgesIgnoringSafeArea(.all)
         }
 
     }
