d8185d4fe50cbf4cf862bc8f9458d2667f31a7b1
Fabrício <guilhermo.fabricio@gmail.com>
2020-04-15 16:42:32 -0300

    Add custom header

--- treinandoSwiftUI/treinandoSwiftUI/ContentView.swift
+++ treinandoSwiftUI/treinandoSwiftUI/ContentView.swift
@@ -10,18 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Viagens")
-            Text("ESPECIAL")
-            Text("BRASIL")
+        GeometryReader { view in
+            VStack {
+                VStack {
+                    Text("viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 40)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 20)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 20)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(/*@START_MENU_TOKEN@*/Color.purple/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(.all)
 
-            List {
-                Text("São Paulo")
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Maragogi")
+                List {
+                    Text("São Paulo")
+                    Text("Rio de Janeiro")
+                    Text("Maragogi")
+                    Text("Ceará")
+                }
             }
         }
+
     }
 }
 
