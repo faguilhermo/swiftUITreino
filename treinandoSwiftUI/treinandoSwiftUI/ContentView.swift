d52e6714befdbc189f4deef101fbc371d4fb94b7
Fabrício <guilhermo.fabricio@gmail.com>
2020-04-15 15:03:06 -0300

    Initial commit

---
+++ treinandoSwiftUI/treinandoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  treinandoSwiftUI
+//
+//  Created by Fabrício Guilhermo on 15/04/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Olá, SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
